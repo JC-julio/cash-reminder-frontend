f323583b581e66c39811df9f7a5289b430a09215
Júlio César <JulioCesarDev@proton.me>
2026-06-06 03:07:19 -0300

    Remove root workspace

--- frontend/app/routes.ts
+++
@@ -1,7 +0,0 @@
-import { type RouteConfig, index, route } from "@react-router/dev/routes";
-
-export default [
-  index("routes/home.tsx"),
-  route("categorias", "routes/categories.tsx"),
-  route("cartao", "routes/card-settings.tsx"),
-] satisfies RouteConfig;
